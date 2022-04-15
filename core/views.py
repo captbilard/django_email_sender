71058a8f7669f3e0e7ef9b5a32c8d1026f09e170
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-04-15 16:08:19 +0100

    Implemented mail sending

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h2> Django</h2>")
+    send_mail(
+        "Test mail",
+        "This is a test mail",
+        "from@example.com",
+        ["to@example.com",],
+        fail_silently=True
+    )
+    return HttpResponse("<h2> Email Sent </h2>")
